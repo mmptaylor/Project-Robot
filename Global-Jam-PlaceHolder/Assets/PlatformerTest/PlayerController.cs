480651289b7805e4779a1e05db96d6d3449cc643
Joshua <joshua.o.m.ross@gmail.com>
2022-01-28 21:48:22 -0500

    added movement for the split screen

---
+++ Global-Jam-PlaceHolder/Assets/PlatformerTest/PlayerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+    float speed = 6;
+    float height = 10;
+    float ad, ws;
+    float jHeight = 4f;
+    bool jump, grounded, ctrlFlag;
+
+    Vector3 overlap;
+    public LayerMask ground;
+
+
+    //holds movement that goes into rb2d
+    float move;
+
+
+    void Start()
+    {
+        ctrlFlag = true;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            ctrlFlag = !ctrlFlag;
+        if (ctrlFlag)
+        {
+            overlap = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.35f, gameObject.transform.position.y);
+            ad = Input.GetAxis("Horizontal");
+            ws = Input.GetAxis("Vertical");
+            jump = Input.GetButton("Jump");
+
+            grounded = Physics2D.OverlapCircle(overlap, 0.5f, ground);
+            //if the jumpButton is pressed and the player is grounded then the velocity of y for rb2d changes to 1 * speed causing the player to jump
+            print(grounded);
+        }
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (ctrlFlag)
+        {
+
+
+
+
+            move = ad * speed;
+            if (jump && grounded)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 1 * height);
+            }
+            rb2d.velocity = new Vector2(move, rb2d.velocity.y);
+            if (rb2d.velocity.y < 0)
+            {
+                rb2d.velocity += Vector2.up * Physics2D.gravity.y * (jHeight - 1) * Time.deltaTime;
+            }
+            else if (rb2d.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb2d.velocity += Vector2.up * Physics2D.gravity.y * (jHeight - 1) * Time.deltaTime;
+            }
+        }
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Color c = new Vector4(0, 1f, 0, 0.25f);
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = c;
+        Gizmos.DrawSphere(overlap, 0.5f);
+
+    }
+
+
+}
---
+++ Global-Jam-PlaceHolder/Assets/TopDownTest/PlayerControllerAI.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerAI : MonoBehaviour
+{
+    bool ctrlFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctrlFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            ctrlFlag = !ctrlFlag;
+
+        if (ctrlFlag)
+        {
+            int layerMask = LayerMask.GetMask("Platform");
+
+
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position + new Vector3(-0.5f, 0), Vector2.left, 0.5f, layerMask);
+
+                if (hit.collider == null)
+                {
+                    transform.position += new Vector3(-0.89f, 0, 0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position + new Vector3(0.5f,0 ), Vector2.right, 0.5f, layerMask);
+
+                if (hit.collider == null)
+                {
+                    transform.position += new Vector3(0.89f, 0, 0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position + new Vector3(0,0.5f), Vector2.up, 0.5f, layerMask);
+
+                if (hit.collider == null)
+                {
+                    transform.position += new Vector3(0f, 0.89f, 0);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position + new Vector3(0,-0.5f), Vector2.down, 0.5f, layerMask);
+
+                if (hit.collider == null)
+                {
+
+                    transform.position += new Vector3(0, -0.89f, 0);
+                }
+            }
+
+        }
+    }
+}
+
+
